34eab342ddddcd9f5f6ea023ccf6a637d6db3dbf
hverified <hverified@gmail.com>
2020-06-11 13:57:17 +0530

    added whitenoise settings and STATIC_ROOT

--- Ecomm/settings.py
+++ Ecomm/settings.py
@@ -29,6 +29,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,7 +112,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # S3 BUCKETS CONFIG
 
